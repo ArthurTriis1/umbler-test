5a662f8f7a0399399cac3dd5f62d3ab9a6c437ac
aluno <arthurfelandrade@gmail.com>
2020-07-05 13:44:45 -0300

    first

---
+++ config/database.js
@@ -0,0 +1,33 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        uri: env('DATABASE_URI'),
+        //uri: "mongodb://mongo_armazem:27017/armazem"
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4b78262b-ff82-4a13-978d-96cf7afeec1c'
+};
